35a777bd23fd8240feff17f1bf4b7723821e12f0
Rabya Tayal <rabyatayal1201@gmail.com>
2023-09-06 18:53:08 +1000

    Completed: Shows list of notebooks now

--- src/app/(protected)/notebooks/page.tsx
+++ src/app/(protected)/notebooks/page.tsx
@@ -1,14 +1,79 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { useAuth } from '@components/auth-provider';
+import React from "react";
+import { useAuth } from "@components/auth-provider";
+import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+const GET_NOTEBOOKS = gql`
+query {
+  notebooks {
+    id
+    title
+    description
+    owner {
+      ... on PartialUser{
+        id 
+        username
+      }
+    }
+  }
+}
+`;
+
+const GET_OWNER = gql`
+  query GetUser($userId: ID!) {
+    owner: user(id: $userId) {
+      id
+      username
+    }
+  }
+`;
+
+interface Notebook {
+  id: string;
+  title: string;
+  description: string;
+  owner: Owner
+}
+
+interface Owner {
+  id: string
+  username: string
+}
+
+interface NotebooksData {
+  notebooks: Notebook[];
+}
 
 export default function Page(): JSX.Element {
-  const { user } = useAuth();
+  
+  
+  const path = usePathname();
+  const { loading, error, data } = useQuery<NotebooksData>(GET_NOTEBOOKS);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
+
+  data?.notebooks.map((notebook) => console.log(notebook.id));
   return (
     <div>
-      <div>USERNAME: {user?.username}</div>
+      {data?.notebooks.map((notebook) => {
+
+
+
+        return (
+          <Link href={`${path}/[id]`} as={`${path}/${notebook.id}`}>
+            
+              
+              <div>{notebook.title}</div>
+              <div>{notebook.description}</div>
+              <div>{notebook.owner.username}</div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
